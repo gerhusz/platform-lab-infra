e6767e571c514fe1c135fec0ce2a485a243d3529
Gergo <gr3g0r14n.0ff1c14l@gmail.com>
2026-04-22 14:34:48 +0200

    VPC init

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -8,3 +8,13 @@ output "state_bucket_arn" {
   description = "ARN of the S3 bucket storing Terraform state"
   value       = aws_s3_bucket.terraform_state.arn
 }
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
+}
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -10,3 +10,15 @@ variable "project_name" {
   type        = string
   default     = "platform-lab"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
